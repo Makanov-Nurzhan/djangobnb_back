f30d897d9b0b57396ebebe70fb60db22a65b840f
Маканов Нуржан Сабырович <makanov_n@cpi.nis.edu.kz>
2024-10-15 17:35:48 +0500

    finish 4 part

--- djangobnb_backend/useraccount/models.py
+++ djangobnb_backend/useraccount/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     avatar = models.ImageField(upload_to='uploads/avatars')
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
