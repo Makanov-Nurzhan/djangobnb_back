767e1c1c7f586dc7cb8938714b481d20cb616952
Маканов Нуржан Сабырович <makanov_n@cpi.nis.edu.kz>
2024-10-18 17:57:09 +0500

    3 part continue

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
+from .form import PropertyForm
 from .models import Property
 from .serializers import PropertiesListSerializer
+
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -15,3 +16,17 @@ def properties_list(request):
     return JsonResponse({
         'data': serializer.data
     })
+
+
+@api_view(['POST', 'FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'success': True})
+    else:
+        print('error', form.errors, form.non_field_errors)
