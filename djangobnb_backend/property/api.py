f6dd1f856aceb542aceb1f47626b8d310b13ba9f
Маканов Нуржан Сабырович <makanov_n@cpi.nis.edu.kz>
2024-11-05 17:48:36 +0500

    finish part 8

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -6,11 +6,12 @@ from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
 from rest_framework_simplejwt.tokens import AccessToken
 from useraccount.models import User
+
+
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
-
     #Auth
     try:
         token = request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
@@ -29,11 +30,49 @@ def properties_list(request):
     #
     is_favorites = request.GET.get('is_favorites')
     landlord_id = request.GET.get('landlord_id', '')
+
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkIn', '')
+    checkout_date = request.GET.get('checkOut', '')
+    bedrooms = request.GET.get('numBedrooms', '')
+    bathrooms = request.GET.get('numBathrooms', '')
+    guests = request.GET.get('numGuests', '')
+
+    print('country', country)
+
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(
+            end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkout_date, end_date__gte=checkin_date)
+        all_matches = []
+
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property_id)
+
+        properties = properties.exclude(id__in=all_matches)
+
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
 
     if is_favorites:
         properties = properties.filter(favorited__in=[user])
+
+    if guests:
+        properties = properties.filter(guests__gte=guests)
+
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+
+    if country:
+        properties = properties.filter(country=country)
+
+    if category and category != 'undefined':
+        properties = properties.filter(category=category)
+
     #
     # Favorites
     print('User:', user)
